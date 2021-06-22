2cfb94e9d23ae5cdafadaf9a3081d992b0d8c0ef
leBomBom <79946658+leBomBom@users.noreply.github.com>
2021-06-22 23:43:07 +0300

    50%

--- script.js
+++ script.js
@@ -1,103 +1,117 @@
-let roundcounter = 1;
-document.getElementById("displaytext").innerHTML  =  "\nRound " + roundcounter + " started\n";
+let counterClickA = 0;
 
 let buttons = document.getElementsByTagName("button"); 
+
 for (var i = 0; i < buttons.length; i++) {
    buttons[i].addEventListener("click", function(event) {
         buttonsControl(event.target);
+        counterClick(event.target);
    }, false);
 }
 
-let counterAction = 0;
-let counterRock = 0;
-let counterWood = 0;
-let counterTomato = 0;
-let counterCoin = 0;
+let counterA = 20;
+let counterB = 20;
 
-let actionPlus = document.querySelector('.action');
-let rockPlus = document.querySelector('.rock');
-let woodPlus = document.querySelector('.wood');
-let tomatoPlus = document.querySelector('.tomato');
-let coinPlus = document.querySelector('.coin');
+let clickCounterA = document.querySelector('.counter-one');
+let clickCounterB = document.querySelector('.counter-two');
+
+let NumberA = document.querySelector('.number-one');
+let oneCircleA = document.querySelector('.circle-one');
+let twoCircleA = document.querySelector('.circle-one-second');
+let treCircleA = document.querySelector('.circle-one-third');
+
+let NumberB = document.querySelector('.number-two');
+let oneCircleB = document.querySelector('.circle-two');
+let twoCircleB = document.querySelector('.circle-two-second');
+let treCircleB = document.querySelector('.circle-two-third');
 
 function buttonsControl(button) {
-
     switch (button.className) {
-        case 'action':
-            counterAction++;
-            button.textContent = counterAction;
-            document.getElementById("displaytext").innerHTML  +=  "+1 action point\n";
+        case 'plus-one':
+            counterA++;
+            NumberA.textContent = counterA;
+            clickCounterA.textContent = counterClickA;
+            soundClick();
+            animateClickA();
+            setCounterA(counterClickA);
             break;
-        case 'rock':
-            counterRock++;
-            button.textContent = counterRock;
-            document.getElementById("displaytext").innerHTML  +=  "+1 rock point\n";
+        case 'minus-one':
+            counterA--;
+            NumberA.textContent = counterA;
+            clickCounterA.textContent = counterClickA;
+            soundClick();
+            animateClickA();
+            setCounterA(counterClickA);
             break;
-        case 'tomato':
-            counterTomato++;
-            button.textContent = counterTomato;
-            document.getElementById("displaytext").innerHTML  +=  "+1 tomato point\n";
+        case 'plus-two':
+            counterB++;
+            NumberB.textContent = counterB;
+            soundClick();
+            animateClickB();
             break;
-        case 'wood':
-            counterWood++;
-            button.textContent = counterWood;
-            document.getElementById("displaytext").innerHTML  +=  "+1 wood point\n";
+        case 'minus-two':
+            counterB--;
+            NumberB.textContent = counterB;
+            soundClick();
+            animateClickB();
             break;
-        case 'coin':
-            counterCoin++;
-            button.textContent = counterCoin;
-            document.getElementById("displaytext").innerHTML  +=  "+1 coin point\n";
-            break;
-        case 'action-plus':
-            counterAction--;
-            actionPlus.textContent = counterAction;
-            document.getElementById("displaytext").innerHTML  +=  "-1 action point\n";
-            break;
-        case 'rock-plus':
-            counterRock--;
-            rockPlus.textContent = counterRock;
-            document.getElementById("displaytext").innerHTML  +=  "-1 rock point\n";
-            break;    
-        case 'tomato-plus':
-            counterTomato--;
-            tomatoPlus.textContent = counterTomato;
-            document.getElementById("displaytext").innerHTML  +=  "-1 tomato point\n";
-            break;
-        case 'wood-plus':
-            counterWood--;
-            woodPlus.textContent = counterWood;
-            document.getElementById("displaytext").innerHTML  +=  "-1 wood point\n";
-            break; 
-        case 'coin-plus':
-            counterCoin--;
-            coinPlus.textContent = counterCoin;
-            document.getElementById("displaytext").innerHTML  +=  "-1 coin point\n";
-            break;                
     };
 }
 
-// reset all for new round
-
-let reset = document.querySelector('.reset');
-reset.addEventListener("click", resetForm);
-
-function resetForm() {
-  
-     roundcounter++;
-
-     counterAction = 0;
-     counterRock = 0;
-     counterTomato = 0;
-     counterWood = 0;
-     counterCoin = 0;
-
-     actionPlus.textContent = counterAction;
-     rockPlus.textContent = counterRock;
-     tomatoPlus.textContent = counterTomato;
-     woodPlus.textContent = counterWood;
-     coinPlus.textContent = counterCoin;
-
-     document.getElementById("displaytext").innerHTML  +=  "Round " + roundcounter + " started\n";
-     document.getElementById("round").innerHTML  =  roundcounter;
-
+function counterClick(button) {
+    if (button.className == 'plus-one' || button.className == 'plus-two') {
+        counterClickA++;
+    } else if (button.className == 'minus-one' || button.className == 'minus-two') {
+        counterClickA--;
+    }
 }
+
+function setCounterA(count) {
+
+    clickCounterA.classList.remove('invisible');
+    setTimeout(fix);
+}
+
+function setTimeout(fix){
+    clickCounterA.classList.add('invisible');
+    counterClickA = 0;
+}, 1000); 
+
+function soundClick() {
+    var audio = new Audio('wav/soft-click.wav');
+    audio.play();
+}
+
+function animateClickA() {
+        oneCircleA.classList.add('animate-circle');
+        twoCircleA.classList.add('animate-circle-two');
+        treCircleA.classList.add('animate-circle-two');
+
+        elements = document.querySelectorAll('.anima');
+        
+            for (let item of elements) {
+                item.addEventListener('animationend', function() {
+                    item.classList.remove('animate-circle');
+                    item.classList.remove('animate-circle-two');
+                    item.classList.remove('animate-circle-two');
+                }); 
+            }
+}
+
+function animateClickB() {
+        oneCircleB.classList.add('animate-circle');
+        twoCircleB.classList.add('animate-circle-two');
+        treCircleB.classList.add('animate-circle-two');
+
+        elements = document.querySelectorAll('.anima');
+        
+            for (let item of elements) {
+                item.addEventListener('animationend', function() {
+                    item.classList.remove('animate-circle');
+                    item.classList.remove('animate-circle-two');
+                    item.classList.remove('animate-circle-two');
+                }); 
+            }
+}
+
+
